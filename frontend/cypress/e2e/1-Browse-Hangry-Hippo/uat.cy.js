7a5b99c8eab67df94db2808c334a34f243f18e16
corrado-k <kebemartey@gmail.com>
2026-05-31 18:58:40 +0000

    feat: enable full CI/CD pipeline and implement browse category acceptance tests

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -4,8 +4,17 @@ describe('Shows Appeteasers', () => {
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click()
   });
   it('selects Appeteasers', () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains('Cheese Burger')
+    cy.contains('Fajita Tacos')
+    cy.contains('Appeteasers').click()
+    cy.contains('Cheese Burger').should('not.exist')
+    cy.contains('Fajita Tacos').should('not.exist')
+
+    cy.contains('Tater Tots')
+    cy.contains('Buffalo Wings')
   });
 });
